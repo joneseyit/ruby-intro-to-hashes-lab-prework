4afcbc8fa336a618625c9e81e2f0c7af7247f28f
joneseyit <joneseyit@protonmail.com>
2018-10-21 00:06:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,8 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => :pieces => 4,
-	                         :names => {},
-	                          :rent_in_dollars => {}
-	  
+	monopoly = {:railroads => {}
+	
 	}
 
 end
